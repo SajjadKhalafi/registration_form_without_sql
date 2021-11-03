64b3291bd5264d84d974daafd37c947638442599
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2021-11-03 17:49:50 +0330

    index.php is done :))

--- index.php
+++ index.php
@@ -1,20 +1,26 @@
 <?php
 const REQUIRE_FIELD_ERROR = "This field is required";
+
 $username = $email = $password = $password_confirm = "";
+
 $messageError = [];
+
 if ($_SERVER['REQUEST_METHOD'] === "POST"){
     $username = post_data('username');
     $email = post_data('email');
     $password = post_data('password');
     $password_confirm = post_data('password_confirm');
+
     if (!$username)
         $messageError['username'] = REQUIRE_FIELD_ERROR;
     elseif (strlen($username) < 6 || strlen($username) > 16)
         $messageError['username'] = "This field must be between 6 and 16 characters long";
+
     if (!$email)
         $messageError['email'] = REQUIRE_FIELD_ERROR;
     elseif (!filter_var($email,FILTER_VALIDATE_EMAIL))
         $messageError['email'] = "Enter your email correctly";
+
     if (!$password)
         $messageError['password'] = REQUIRE_FIELD_ERROR;
     if (!$password_confirm)
@@ -22,6 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST"){
     elseif ($password && strcmp($password,$password_confirm) !== 0)
         $messageError['password_confirm'] = "Please repeat your password correctly";
 }
+
 function post_data($field){
     if (!isset($_POST[$field])){
         return false;
@@ -48,76 +55,76 @@ function post_data($field){
 </head>
 <body>
 
-<div class="main">
+    <div class="main">
 
-    <!-- Sign up form -->
-    <section class="signup">
-        <div class="container">
-            <div class="signup-content">
-                <div class="signup-form">
-                    <h2 class="form-title">Sign up</h2>
-                    <form action="<?= $_SERVER['PHP_SELF']; ?>" method="post" class="register-form" id="register-form" novalidate>
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                    <input type="text" name="username" id="name" placeholder="Your Name" class="form-control <?= isset($messageError['username']) ? 'is-invalid' : '' ; ?> " value="<?= $username; ?>"/>
-                                    <div class="invalid-feedback">
-                                        <?= $messageError['username'] ?? ""; ?>
+        <!-- Sign up form -->
+        <section class="signup">
+            <div class="container">
+                <div class="signup-content">
+                    <div class="signup-form">
+                        <h2 class="form-title">Sign up</h2>
+                        <form action="<?= $_SERVER['PHP_SELF']; ?>" method="post" class="register-form" id="register-form" novalidate>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                        <input type="text" name="username" id="name" placeholder="Your Name" class="form-control <?= isset($messageError['username']) ? 'is-invalid' : '' ; ?> " value="<?= $username; ?>"/>
+                                        <div class="invalid-feedback">
+                                            <?= $messageError['username'] ?? ""; ?>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    <label for="email"><i class="zmdi zmdi-email"></i></label>
-                                    <input type="email" name="email" id="email" placeholder="Your Email" class="form-control <?= isset($messageError['email']) ? 'is-invalid' : '' ; ?> " value="<?= $email; ?>"/>
-                                    <div class="invalid-feedback">
-                                        <?= $messageError['email'] ?? ""; ?>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="email"><i class="zmdi zmdi-email"></i></label>
+                                        <input type="email" name="email" id="email" placeholder="Your Email" class="form-control <?= isset($messageError['email']) ? 'is-invalid' : '' ; ?> " value="<?= $email; ?>"/>
+                                        <div class="invalid-feedback">
+                                            <?= $messageError['email'] ?? ""; ?>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    <label for="pass"><i class="zmdi zmdi-lock"></i></label>
-                                    <input type="password" name="password" id="pass" placeholder="Password" class="form-control <?= isset($messageError['password']) ? 'is-invalid' : '' ; ?> " value="<?= $password; ?>"/>
-                                    <div class="invalid-feedback">
-                                        <?= $messageError['password'] ?? ""; ?>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+                                        <input type="password" name="password" id="pass" placeholder="Password" class="form-control <?= isset($messageError['password']) ? 'is-invalid' : '' ; ?> " value="<?= $password; ?>"/>
+                                        <div class="invalid-feedback">
+                                            <?= $messageError['password'] ?? ""; ?>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    <label for="re_pass"><i class="zmdi zmdi-lock-outline"></i></label>
-                                    <input type="password" name="password_confirm" id="re_pass" placeholder="Repeat your password" class="form-control <?= isset($messageError['password_confirm']) ? 'is-invalid' : '' ; ?> " value="<?= $password_confirm; ?>"/>
-                                    <div class="invalid-feedback">
-                                        <?= $messageError['password_confirm'] ?? ""; ?>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="re_pass"><i class="zmdi zmdi-lock-outline"></i></label>
+                                        <input type="password" name="password_confirm" id="re_pass" placeholder="Repeat your password" class="form-control <?= isset($messageError['password_confirm']) ? 'is-invalid' : '' ; ?> " value="<?= $password_confirm; ?>"/>
+                                        <div class="invalid-feedback">
+                                            <?= $messageError['password_confirm'] ?? ""; ?>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="form-group">
-                            <input type="checkbox" name="agree-term" id="agree-term" class="agree-term checkbox" />
-                            <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
-                        </div>
-                        <div class="form-group form-button">
-                            <input type="submit" name="signup" id="signup" class="form-submit delete" value="Register" disabled="disabled"/>
-                        </div>
-                    </form>
-                </div>
-                <div class="signup-image">
-                    <figure><img src="frontEnd/images/signup-image.jpg" alt="sing up image"></figure>
-                    <a href="#" class="signup-image-link">I am already member</a>
+                            <div class="form-group">
+                                <input type="checkbox" name="agree-term" id="agree-term" class="agree-term checkbox" />
+                                <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
+                            </div>
+                            <div class="form-group form-button">
+                                <input type="submit" name="signup" id="signup" class="form-submit delete" value="Register" disabled="disabled"/>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="signup-image">
+                        <figure><img src="frontEnd/images/signup-image.jpg" alt="sing up image"></figure>
+                        <a href="#" class="signup-image-link">I am already member</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-</div>
+        </section>
+    </div>
 
 <!-- JS -->
 <script src="frontEnd/vendor/jquery/jquery.min.js"></script>
